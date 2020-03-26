8ba78d317f57ecfe8901c90cc8df76bc5eea674a
Cody Brown <cody6lefever@gmail.com>
2020-03-26 16:43:57 -0700

    actor models routes and tests done

--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,8 @@
 const chance = require('chance').Chance();
 const Studio = require('../lib/models/Studio');
+const Actor = require('../lib/models/Actor');
 
-module.exports = async({ studiosToCreate = 5 } = {}) => {
+module.exports = async({ studiosToCreate = 5, actorsToCreate = 5, } = {}) => {
 
   const studios = await Studio.create([...Array(studiosToCreate)].map(() => ({
     name: `${chance.animal()}`,
@@ -10,6 +11,12 @@ module.exports = async({ studiosToCreate = 5 } = {}) => {
       state: `${chance.word()}`,
       country: `${chance.word()}`
     }
+  })));
+
+  const actors = await Actor.create([...Array(actorsToCreate)].map(() => ({
+    name: `${chance.animal()}`,
+    dob: chance.date(),
+    pob: `${chance.word()}`
   })));
 
 
