fd554217a7601980f5ebdc496cb1f42200ad713a
Cody Brown <cody6lefever@gmail.com>
2020-03-26 15:35:22 -0700

    one test done and a ton more to go

--- __tests__/studio.test.js
+++ __tests__/studio.test.js
@@ -1,12 +1,31 @@
 const request = require('supertest');
 const app = require('../lib/app');
+require('../db/data-helpers');
+
 
 describe('studio routes', () => {
   it('creates a studio', () => {
     return request(app)
       .post('/api/v1/studios')
       .send({
-        
+        name: 'Warn A Brotha',
+        address: {
+          city: 'LA',
+          state: 'CA',
+          country: 'the best'
+        }
       })
-  })
-})
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Warn A Brotha',
+          address: {
+            city: 'LA',
+            state: 'CA',
+            country: 'the best'
+          },
+          __v: 0
+        });
+      });
+  });
+});
