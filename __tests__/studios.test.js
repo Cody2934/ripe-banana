e78e2085011f9e5dabec361b13decaa60d62354a
Cody Brown <cody6lefever@gmail.com>
2020-03-26 16:20:30 -0700

    studio routes built and tested

--- __tests__/studios.test.js
+++ __tests__/studios.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const app = require('../lib/app');
-const { getStudio } = require('../db/data-helpers');
+const { getStudio, getStudios } = require('../db/data-helpers');
 
 
 describe('studio routes', () => {
@@ -31,11 +31,42 @@ describe('studio routes', () => {
 
   it('gets a studio by id', async() => {
     const studio = await getStudio();
-
     return request(app)
       .get(`/api/v1/studios/${studio._id}`)
       .then(res => {
         expect(res.body).toEqual(studio);
       });
   });
+
+  it('gets all studios', async() => {
+    const studios = await getStudios();
+    return request(app)
+      .get('/api/v1/studios')
+      .then(res => {
+        expect(res.body).toEqual(studios);
+      });
+  });
+
+  it('updates a studio by id', async() => {
+    const studio = await getStudio();
+    return request(app)
+      .patch(`/api/v1/studios/${studio._id}`)
+      .send({ name: 'shiddy studios' })
+      .then(res => {
+        expect(res.body).toEqual({
+          ...studio,
+          name: 'shiddy studios'
+        });
+      });
+  });
+
+  it('deletes a studio by id', async() => {
+    const studio = await getStudio();
+    return request(app)
+      .delete(`/api/v1/studios/${studio._id}`)
+      .then(res => {
+        expect(res.body).toEqual(studio);
+      });
+  });
+
 });
