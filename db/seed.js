134650a7e2efcb137b3600f25a4a7708a32d5a2b
Cody Brown <cody6lefever@gmail.com>
2020-03-26 17:00:28 -0700

    reviewers was really hard to type for some reason but routes model and test are done for them

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,9 @@
 const chance = require('chance').Chance();
 const Studio = require('../lib/models/Studio');
 const Actor = require('../lib/models/Actor');
+const Reviewer = require('../lib/models/Reviewer');
 
-module.exports = async({ studiosToCreate = 5, actorsToCreate = 5, } = {}) => {
+module.exports = async({ studiosToCreate = 5, actorsToCreate = 5, reviewersToCreate = 5 } = {}) => {
 
   const studios = await Studio.create([...Array(studiosToCreate)].map(() => ({
     name: `${chance.animal()}`,
@@ -17,6 +18,11 @@ module.exports = async({ studiosToCreate = 5, actorsToCreate = 5, } = {}) => {
     name: `${chance.animal()}`,
     dob: chance.date(),
     pob: `${chance.word()}`
+  })));
+
+  const reviewers = await Reviewer.create([...Array(reviewersToCreate)].map(() => ({
+    name: `${chance.word()}`,
+    company: `${chance.animal()}`
   })));
 
 
