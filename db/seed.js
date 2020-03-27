30cc2b36df780b591184aa52b31a90bfd2661914
Cody Brown <cody6lefever@gmail.com>
2020-03-27 11:40:37 -0700

    film model routes and tests are all good

--- db/seed.js
+++ db/seed.js
@@ -1,9 +1,11 @@
 const chance = require('chance').Chance();
 const Studio = require('../lib/models/Studio');
 const Actor = require('../lib/models/Actor');
+const Film = require('../lib/models/Film');
+
 const Reviewer = require('../lib/models/Reviewer');
 
-module.exports = async({ studiosToCreate = 5, actorsToCreate = 5, reviewersToCreate = 5 } = {}) => {
+module.exports = async({ studiosToCreate = 5, actorsToCreate = 5, reviewersToCreate = 5, filmsToCreate = 5 } = {}) => {
 
   const studios = await Studio.create([...Array(studiosToCreate)].map(() => ({
     name: `${chance.animal()}`,
@@ -25,5 +27,14 @@ module.exports = async({ studiosToCreate = 5, actorsToCreate = 5, reviewersToCre
     company: `${chance.animal()}`
   })));
 
+  const films = await Film.create([...Array(filmsToCreate)].map(() => ({
+    title: chance.animal(),
+    studioId: chance.pickone(studios)._id,
+    released: chance.integer({ min: 1900, max: 2020 }),
+    cast: [{
+      role: `${chance.word()}`,
+      actorId: chance.pickone(actors)._id
+    }]
+  })));
 
 };
